1c7c4faaaf6057b963c8ab7929e20699671f433c
Ancij <ancij.yt@gmail.com>
2014-08-05 22:46:33 -0430

    Agregar Estilos, y agregar Lagarto y Spock

--- Tarea-clase1/js/ppt.js
+++ Tarea-clase1/js/ppt.js
@@ -1,63 +1,100 @@
+var piedra = 0;
+var papel = 1;
+var tijera = 2;
+var lagarto = 3;
+var spock = 4;
+
+var opciones = ['Piedra', 'Papel', 'Tijera', 'Lagarto', 'Spock'];
+var mensaje = document.getElementById('mensaje');
+var estado = document.getElementById('estado');
+
 function ram(min, max){
 	var numero = Math.floor( Math.random() * (max - min + 1) + min );
 	return numero;
 }
 
+function decir(mensaje, resul){
+	var ganar = 2;
+	var empate = 1;
+	var perder = 0;
+
+	estado.innerHTML = mensaje;
+	if(resul == 0){
+		estado.classList.remove();
+		estado.className = 'lose';
+	}else if(resul == 1){
+		estado.classList.remove();
+		estado.className = 'draw';
+	}else{
+		estado.classList.remove();
+		estado.className = 'win';
+	}
+}
+
 function jugar(){
-
-	var piedra = 0;
-	var papel = 1;
-	var tijera = 2;
-
-	var opciones = ['Piedra', 'Papel', 'Tijera'];
-	var opcionMaquina = ram(0,2);
 	var opcionUsuario = document.getElementById('selec').value;
-	var mensaje = document.getElementById('mensaje');
-
+	var opcionMaquina = ram(0,4);
 	mensaje.innerHTML = opciones[opcionUsuario] + ' vs ' + opciones[opcionMaquina];
 
-	if (opcionUsuario == piedra){
-		if (opcionMaquina == piedra){
-
-			mensaje.innerHTML = mensaje.innerHTML + '<br/>' + '<h2>Empate!</h2>';
-
+	if(opcionUsuario == piedra){
+		if(opcionMaquina == piedra){
+			decir('Empate', 1);
 		}else if(opcionMaquina == papel){
-
-			mensaje.innerHTML = mensaje.innerHTML + '<br/>' + '<h2>Perdiste!</h2>';
-
+			decir('Perdiste', 0);
 		}else if(opcionMaquina == tijera){
-
-			mensaje.innerHTML = mensaje.innerHTML + '<br/>' + '<h2>Ganaste!</h2>';
-
+			decir('Ganaste', 2);
+		}else if(opcionMaquina == lagarto){
+			decir('Ganaste', 2);
+		}else{
+			decir('Perdiste', 0);
 		}
-	}else if (opcionUsuario == papel){
-		if (opcionMaquina == piedra){
-
-			mensaje.innerHTML = mensaje.innerHTML + '<br/>' + '<h2>Ganaste!</h2>';
-
+	}else if(opcionUsuario == papel){
+		if(opcionMaquina == piedra){
+			decir('Ganaste', 2);
 		}else if(opcionMaquina == papel){
-
-			mensaje.innerHTML = mensaje.innerHTML + '<br/>' + '<h2>Empate!</h2>';
-
+			decir('Empate', 1);
 		}else if(opcionMaquina == tijera){
-
-			mensaje.innerHTML = mensaje.innerHTML + '<br/>' + '<h2>Perdiste!</h2>';
-
+			decir('Perdiste', 0);
+		}else if(opcionMaquina == lagarto){
+			decir('Perdiste', 0);
+		}else{
+			decir('Ganaste', 2);
 		}
-	}else {
-		if (opcionMaquina == piedra){
-
-			mensaje.innerHTML = mensaje.innerHTML + '<br/>' + '<h2>Perdiste!</h2>';
-
+	}else if(opcionUsuario == tijera){
+		if(opcionMaquina == piedra){
+			decir('Perdiste', 0);
 		}else if(opcionMaquina == papel){
-
-			mensaje.innerHTML = mensaje.innerHTML + '<br/>' + '<h2>Ganaste!</h2>';
-
+			decir('Ganaste', 2);
 		}else if(opcionMaquina == tijera){
-
-			mensaje.innerHTML = mensaje.innerHTML + '<br/>' + '<h2>Empate!</h2>';
-
+			decir('Empate', 1);
+		}else if(opcionMaquina == lagarto){
+			decir('Ganaste', 2);
+		}else{
+			decir('Perdiste', 0);
+		}
+	}else if(opcionUsuario == lagarto){
+		if(opcionMaquina == piedra){
+			decir('Perdiste', 0);
+		}else if(opcionMaquina == papel){
+			decir('Ganaste', 2);
+		}else if(opcionMaquina == tijera){
+			decir('Perdiste', 0);
+		}else if(opcionMaquina == lagarto){
+			decir('Empate', 1);
+		}else{
+			decir('Ganaste', 2);
+		}
+	}else{
+		if(opcionMaquina == piedra){
+			decir('Ganaste', 2);
+		}else if(opcionMaquina == papel){
+			decir('Perdiste', 0);
+		}else if(opcionMaquina == tijera){
+			decir('Ganaste', 2);
+		}else if(opcionMaquina == lagarto){
+			decir('Perdiste', 0);
+		}else{
+			decir('Empate', 1);
 		}
 	}
-
 }
