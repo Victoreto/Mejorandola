fb5f815476d21b8eb5b8ec09ce046d7edbdb337c
Ancij <ancij.yt@gmail.com>
2014-08-10 01:39:15 -0430

    Agregar mensaje con estilos

--- Semana1/pesoMarte/main.js
+++ Semana1/pesoMarte/main.js
@@ -1,12 +1,14 @@
 function start(){
 	var peso = document.getElementById('peso');
 	var divResul = document.getElementById('pesoEnMarte');
+    var mensaje = document.getElementById('mensaje');
 	peso = parseInt(peso.value);
     if(peso === 0 || peso === ''){
         alert('Porfavor ingrese su peso');
     }else{
         var pesoEnMarte = (peso/9.81) * 3.771;
         pesoEnMarte = Math.round(pesoEnMarte * 100) / 100;
-        divResul.innerHTML = pesoEnMarte;
+        mensaje.className = 'alert alert-success';
+        mensaje.innerHTML = '<strong>Tu peso en marte sería: ' + pesoEnMarte + ' Kg.</strong>';
     }
 }
