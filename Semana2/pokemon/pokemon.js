a8f5169562a51b34623f26054130a8e1608a08f2
Kildare Lauser <skye@MacBook.(none)>
2014-08-17 01:00:18 -0430

    Agregar Grito

--- Semana2/pokemon/pokemon.js
+++ Semana2/pokemon/pokemon.js
@@ -1,22 +1,25 @@
-function Pokemon(n,t,v,a){
+function Pokemon(n,t,v,a,g){
 	this.nombre = n;
 	this.tipo = t;
 	this.vida = v;
 	this.ataque = a;
+	this.grito = g;
 	this.imagen = function (){
 		return this.nombre + '.png'
 	}
 }
 
-var pikachu = new Pokemon("Pikachu", "Electrico", 20, 30);
+var pikachu = new Pokemon("Pikachu", "Electrico", 20, 30, 'Pika!!');
 var nombre = document.getElementById('nombre');
 var tipo = document.getElementById('tipo');
 var vida = document.getElementById('vida');
 var ataque = document.getElementById('ataque');
 var imagen = document.getElementById('imagen');
+var grito = document.getElementById('grito');
 
 nombre.innerHTML = pikachu.nombre;
 tipo.innerHTML = 'Tipo: ' + pikachu.tipo;
 vida.innerHTML = 'Vida: ' + pikachu.vida;
 ataque.innerHTML = 'Ataque ' + pikachu.ataque;
-imagen.innerHTML = '<img src="'+ pikachu.imagen() +'" alt="Pokemon" id="imagen">'
+imagen.innerHTML = '<img src="'+ pikachu.imagen() +'" alt="Pokemon" id="imagen">';
+grito.innerHTML = 'Grito: ' + pikachu.grito;
